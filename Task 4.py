6365fa3876624b13eb83b7b92397752dab5cdce2
Srihitha2782 <85793086+Srihitha2782@users.noreply.github.com>
2021-06-15 15:44:56 +0530

    Add files via upload

---
+++ Task 4.py
@@ -0,0 +1,34 @@
+
+# Creating three variables a,b,c
+print (" DAY 4 TASK")
+a=b=c=500
+print("data type of a,b,c :",type (a))
+print("divide a value by 10")
+print(a,"/","10","=",a/10)
+print("multiply b value by 50")
+print(b,"*","50","=",b*50)
+print("add c value by 60")
+print(c,"+","60","=",c+60)
+
+# Creating a String variable of 5 characters
+code="278hs"
+print("String",code)
+print("length of the string:",len(code))
+s1="8"
+s2="R"
+c1=code.replace(s1,s2)
+print("New string:",c1)
+
+# creating two values(a,b) of int, float data type
+a,b=15,7.6
+print("a=",a,"b=",b)
+print("data type of a :",type(a))
+print("data type of b :",type(b))
+a=float(a)
+b=int(b)
+print("value of a and b after type conversation")
+print("a=",a,"b=",b)
+print("data type of a after type conversation:",type(a))
+print("data type of b after type conversation:",type(b))
+print(" TASK COMPLETED ")
+
