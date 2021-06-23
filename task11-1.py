8609f73a1a61e08ab75e05da2034abfb7454a85a
Srihitha2782 <85793086+Srihitha2782@users.noreply.github.com>
2021-06-23 17:31:48 +0530

    Add files via upload

---
+++ task11-1.py
@@ -0,0 +1,13 @@
+
+
+import scratch_13
+scratch_13.mymodule('mag')
+scratch_13.l[1]=8
+print("After updating : ",scratch_13.l)
+import random
+print(random.randint(1,100))
+print(random.random())
+
+import sys
+sys.path.append( 'C:/Users/Srihitha/downloads')
+print(sys.path)
