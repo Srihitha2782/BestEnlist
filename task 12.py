90daaeeda0cbe82edf3425d7ef7cc9d7bf3a9965
Srihitha2782 <85793086+Srihitha2782@users.noreply.github.com>
2021-06-24 19:53:17 +0530

    Add files via upload

---
+++ task 12.py
@@ -0,0 +1,7 @@
+file 1 = open("30days 30hours operations", "w")
+file 1.write("\n I have completed 10 days successfully.")
+file 1.close()
+
+file 2 = open("30 days 30hours operations", "a")
+file 2.write("\n - Hanmandla Srihitha Reddy")
+file2.close()
